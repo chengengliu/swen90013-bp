cb7163b54e480967d93ee741778723224bf207a3
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2020-05-04 05:39:07 +0800

    trying give service impl the handling functionality

--- src/main/java/org/apromore/plugin/services/impl/MyServiceImpl.java
+++ src/main/java/org/apromore/plugin/services/impl/MyServiceImpl.java
@@ -1,11 +1,20 @@
 package org.apromore.plugin.services.impl;
 
 import org.apromore.plugin.services.MyService;
+
+import java.io.File;
 import java.text.DateFormat;
 import java.util.Date;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service("myService")
 @Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
@@ -15,4 +24,9 @@ public class MyServiceImpl implements MyService {
 		return DateFormat.getDateInstance().format(new Date());
 	}
 
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	@ResponseBody
+	public String getFileInquiry(HttpServletRequest request){
+		return null;
+	}
 }
