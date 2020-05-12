ceec7e00a8a9b8635de78546401c16e81fe664cf
Zhuo FENG <zhuo.feng@student.unimelb.edu.au>
2020-05-13 03:22:13 +1000

    fix: fix IndexIntegrationTest for the code style check

--- src/test/java/org/apromore/plugin/services/impl/IndexIntegrationTest.java
+++ src/test/java/org/apromore/plugin/services/impl/IndexIntegrationTest.java
@@ -4,34 +4,44 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
 
+/**
+ * Integration Test for the index page
+ */
 public class IndexIntegrationTest {
-  private static WebDriver driver;
+    private static WebDriver driver;
 
-  /**
-   * Setup selenium
-   * @see swen90013-bp.atlassian.net/wiki/spaces/BPM/pages/129269866/Integration+Test#Run-the-Tests
-   */
-  @BeforeClass
-  public static void setup () {
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-  }
+    /**
+     * Setup Selenium
+     */
+    @BeforeClass
+    public static void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
-  @AfterClass
-  public static void clean () {
-    driver.quit();
-  }
+    /**
+     * Quit Selenium WebDriver
+     */
+    @AfterClass
+    public static void clean() {
+        driver.quit();
+    }
 
-  @Test
-  public void shouldFindUploadButton () {
-    driver.get("https://bp.omjad.as/preprocessing-plugin/");
+    /**
+     * Test that there should be an upload button on index page
+     */
+    @Test
+    public void shouldFindUploadButton() {
+        driver.get("https://bp.omjad.as/preprocessing-plugin/");
 
-    assertEquals("Upload", driver.findElement(By.cssSelector("button")).getText());
-  }
+        WebElement button = driver.findElement(By.cssSelector("button"));
+        assertEquals("Upload", button.getText());
+    }
 }
