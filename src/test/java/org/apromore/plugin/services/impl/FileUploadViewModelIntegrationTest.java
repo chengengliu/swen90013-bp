adb14d27012603d95c4af84987aeee996277fee1
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2020-06-07 19:59:19 +0800

    fix: code refactor

--- src/test/java/org/apromore/plugin/services/impl/FileUploadViewModelIntegrationTest.java
+++ src/test/java/org/apromore/plugin/services/impl/FileUploadViewModelIntegrationTest.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import static org.junit.Assert.assertNotNull;
 
 /**
