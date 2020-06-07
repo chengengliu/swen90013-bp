d93075ba06d7997ea36067e2cb10f50246ca47ee
l-trethewey <43433271+l-trethewey@users.noreply.github.com>
2020-06-07 16:48:37 +1000

    feat: multiple queries

--- src/main/java/org/apromore/plugin/JoinPanelViewModel.java
+++ src/main/java/org/apromore/plugin/JoinPanelViewModel.java
@@ -22,166 +22,203 @@ import org.zkoss.zk.ui.select.annotation.WireVariable;
 /**
  * Model for the join panel.
  */
+/**
+ * @author LachlanT
+ *
+ */
+/**
+ * @author LachlanT
+ *
+ */
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class JoinPanelViewModel {
 
-    private List<JoinQueryModel> joinQueryModels;
-    private List<String> filenames;
-    private List<String> joins;
+	private List<JoinQueryModel> joinQueryModels;
+	private List<String> filenames;
+	private List<String> joins;
 
-    @WireVariable
-    private FileHandlerService fileHandlerService;
+	@WireVariable
+	private FileHandlerService fileHandlerService;
 
-    @WireVariable
-    private Transaction transactionService;
+	@WireVariable
+	private Transaction transactionService;
 
-    /**
-     * Initialise.
-     */
-    @Init
-    public void init() {
-        filenames = new ArrayList<String>();
-        joinQueryModels = new ArrayList<JoinQueryModel>();
-        joins = new ArrayList<String>();
-        joins.add("INNER JOIN");
-        joins.add("LEFT JOIN");
-        joins.add("RIGHT JOIN");
-        joins.add("FULL OUTER JOIN");
-        joinQueryModels.add(new JoinQueryModel());
-    }
+	/**
+	 * Initialise.
+	 */
+	@Init
+	public void init() {
+		filenames = new ArrayList<String>();
+		joinQueryModels = new ArrayList<JoinQueryModel>();
+		joins = new ArrayList<String>();
+		joins.add("INNER JOIN");
+		joins.add("LEFT JOIN");
+		joins.add("RIGHT JOIN");
+		joins.add("FULL OUTER JOIN");
+		joinQueryModels.add(new JoinQueryModel());
+	}
 
-    /**
-     * onSelect command for table A being selected.
-     * @param index Index of join query model.
-     */
-    @Command("onTableASelected")
-    @NotifyChange("joinQueryModels")
-    public void onTableASelected(@BindingParam("index") int index) {
-        String selectedTableAName =
-                joinQueryModels.get(index).getSelectedTableA();
-        List<List<String>> resultsList;
+	/**
+	 * onSelect command for table A being selected.
+	 * @param index Index of join query model.
+	 */
+	@Command("onTableASelected")
+	@NotifyChange("joinQueryModels")
+	public void onTableASelected(@BindingParam("index") int index) {
+		String selectedTableAName =
+				joinQueryModels.get(index).getSelectedTableA();
+		List<List<String>> resultsList;
 
-        try {
-            resultsList = transactionService.getSnippet(selectedTableAName, 1);
-            joinQueryModels.get(index).getTableAKeys().clear();
-            joinQueryModels.get(index).setTableAKeys(resultsList.get(0));
+		try {
+			resultsList = transactionService.getSnippet(selectedTableAName, 1);
+			joinQueryModels.get(index).getTableAKeys().clear();
+			joinQueryModels.get(index).setTableAKeys(resultsList.get(0));
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * onSelect command for table B being selected.
-     * @param index Index of join query model.
-     */
-    @Command("onTableBSelected")
-    @NotifyChange("joinQueryModels")
-    public void onTableBSelected(@BindingParam("index") int index) {
-        String selectedTableBName =
-                joinQueryModels.get(index).getSelectedTableB();
-        List<List<String>> resultsList;
+	/**
+	 * onSelect command for table B being selected.
+	 * @param index Index of join query model.
+	 */
+	@Command("onTableBSelected")
+	@NotifyChange("joinQueryModels")
+	public void onTableBSelected(@BindingParam("index") int index) {
+		String selectedTableBName =
+				joinQueryModels.get(index).getSelectedTableB();
+		List<List<String>> resultsList;
 
-        try {
-            resultsList = transactionService.getSnippet(selectedTableBName, 1);
-            joinQueryModels.get(index).getTableBKeys().clear();
-            joinQueryModels.get(index).setTableBKeys(resultsList.get(0));
+		try {
+			resultsList = transactionService.getSnippet(selectedTableBName, 1);
+			joinQueryModels.get(index).getTableBKeys().clear();
+			joinQueryModels.get(index).setTableBKeys(resultsList.get(0));
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Submit join query.
-     *
-     * @param index Index of join query model
-     */
-    @Command("submitQuery")
-    public void submitQuery(@BindingParam("index") int index) {
+	/**
+	 * Submit join query.
+	 *
+	 */
+	@Command("submitQuery")
+	public void submitQuery() {
 
-        List<String> joinTableInfo = joinQueryModels.get(index).submit();
-        List<List<String>> joinInfo = new ArrayList<>();
 
-        joinInfo.add(joinTableInfo);
+		//List<String> joinTableInfo = joinQueryModels.get(index).submit();
+		List<List<String>> totalJoinQuery = new ArrayList<>();
 
-        List<List<String>> resultsList = null;
-        try {
+		for(JoinQueryModel j: joinQueryModels) {
+			List<String> joinQueryAttributes = j.submit();
+			totalJoinQuery.add(joinQueryAttributes);
+			System.out.println(j);
+		}
 
-            resultsList = transactionService.join(joinInfo, 10);
-            Map<String,Object> args = new HashMap<String,Object>();
-            args.put("resultsList", resultsList);
-            BindUtils.postGlobalCommand(null, null, "onTableClick", args);
 
-        } catch (SQLException e) {
+		List<List<String>> resultsList = null;
+		try {
 
-            e.printStackTrace();
-        }
-    }
+			resultsList = transactionService.join(totalJoinQuery, 10);
+			Map<String,Object> args = new HashMap<String,Object>();
+			args.put("resultsList", resultsList);
+			BindUtils.postGlobalCommand(null, null, "onTableClick", args);
 
-    /**
-     * Update list of file names. Can be called globally.
-     *
-     * @param filenames list of uploaded file names
-     */
-    @GlobalCommand
-    @NotifyChange("filenames")
-    public void newFileUpload(@BindingParam("filenames")
-        List<String> filenames) {
-        this.filenames = filenames;
-    }
+		} catch (SQLException e) {
 
-    /**
-     * Get joinQueryModels.
-     *
-     * @return joinQueryModels joinQueryModels
-     */
-    public List<JoinQueryModel> getJoinQueryModels() {
-        return joinQueryModels;
-    }
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Set joinQueryModels.
-     *
-     * @param joinQueryModels joinQueryModels
-     */
-    public void setJoinQueryModels(List<JoinQueryModel> joinQueryModels) {
-        this.joinQueryModels = joinQueryModels;
-    }
 
-    /**
-     * Get joins.
-     *
-     * @return joins joins
-     */
-    public List<String> getJoins() {
-        return joins;
-    }
 
-    /**
-     * Set joins.
-     *
-     * @param joins joins
-     */
-    public void setJoins(List<String> joins) {
-        this.joins = joins;
-    }
+	/**
+	 * Add a new joinQueryModel.
+	 *
+	 */
+	@Command("addJoinQuery")
+	@NotifyChange("joinQueryModels")
+	public void addJoinQuery() {
+		joinQueryModels.add(new JoinQueryModel());
+	}
 
-    /**
-     * Get filenames.
-     *
-     * @return filenames filenames
-     */
-    public List<String> getFilenames() {
-        return filenames;
-    }
 
-    /**
-     * Set filenames.
-     *
-     * @param filenames filenames
-     */
-    public void setFilenames(List<String> filenames) {
-        this.filenames = filenames;
-    }
+	/**
+	 * Remove the specified joinQueryModel.
+	 *
+	 * @param index Index of joinQueryModel in joniQueryModels to remove
+	 */
+	@Command("removeJoinQuery")
+	@NotifyChange("joinQueryModels")
+	public void removeJoinQuery(@BindingParam("index") int index) {
+		joinQueryModels.remove(index);
+	}
+
+	/**
+	 * Update list of file names. Can be called globally.
+	 *
+	 * @param filenames list of uploaded file names
+	 */
+	@GlobalCommand
+	@NotifyChange("filenames")
+	public void newFileUpload(@BindingParam("filenames")
+	List<String> filenames) {
+		this.filenames = filenames;
+	}
+
+	/**
+	 * Get joinQueryModels.
+	 *
+	 * @return joinQueryModels joinQueryModels
+	 */
+	public List<JoinQueryModel> getJoinQueryModels() {
+		return joinQueryModels;
+	}
+
+	/**
+	 * Set joinQueryModels.
+	 *
+	 * @param joinQueryModels joinQueryModels
+	 */
+	public void setJoinQueryModels(List<JoinQueryModel> joinQueryModels) {
+		this.joinQueryModels = joinQueryModels;
+	}
+
+	/**
+	 * Get joins.
+	 *
+	 * @return joins joins
+	 */
+	public List<String> getJoins() {
+		return joins;
+	}
+
+	/**
+	 * Set joins.
+	 *
+	 * @param joins joins
+	 */
+	public void setJoins(List<String> joins) {
+		this.joins = joins;
+	}
+
+	/**
+	 * Get filenames.
+	 *
+	 * @return filenames filenames
+	 */
+	public List<String> getFilenames() {
+		return filenames;
+	}
+
+	/**
+	 * Set filenames.
+	 *
+	 * @param filenames filenames
+	 */
+	public void setFilenames(List<String> filenames) {
+		this.filenames = filenames;
+	}
 }
--- src/main/java/org/apromore/plugin/models/JoinQueryModel.java
+++ src/main/java/org/apromore/plugin/models/JoinQueryModel.java
@@ -9,163 +9,163 @@ import org.apache.commons.io.FilenameUtils;
  * Model for a join query.
  */
 public class JoinQueryModel {
-    private String selectedTableA;
-    private String selectedTableB;
-    private String selectedJoin;
-    private String selectedKeyA;
-    private String selectedKeyB;
+	private String selectedTableA;
+	private String selectedTableB;
+	private String selectedJoin;
+	private String selectedKeyA;
+	private String selectedKeyB;
 
-    private List<String> tableAKeys;
-    private List<String> tableBKeys;
+	private List<String> tableAKeys;
+	private List<String> tableBKeys;
 
-    /**
-     * Constructor.
-     */
-    public JoinQueryModel() {
-        tableAKeys = new ArrayList<String>();
-        tableBKeys = new ArrayList<String>();
-    }
+	/**
+	 * Constructor.
+	 */
+	public JoinQueryModel() {
+		tableAKeys = new ArrayList<String>();
+		tableBKeys = new ArrayList<String>();
+	}
 
-    /**
-     * Send the join query to Impala. Not currently implemented.
-     *
-     * @return Get the join table Info
-     */
-    public List<String> submit() {
-        List<String> tableQuery = new ArrayList<>();
-        tableQuery.add(FilenameUtils.removeExtension(selectedTableA));
-        tableQuery.add(selectedKeyA);
-        tableQuery.add(FilenameUtils.removeExtension(selectedTableB));
-        tableQuery.add(selectedKeyB);
-        tableQuery.add(selectedJoin);
+	/**
+	 * Send the join query to Impala. Not currently implemented.
+	 *
+	 * @return Get the join table Info
+	 */
+	public List<String> submit() {
+		List<String> tableQuery = new ArrayList<>();
+		tableQuery.add(FilenameUtils.removeExtension(selectedTableA));
+		tableQuery.add(selectedKeyA);
+		tableQuery.add(FilenameUtils.removeExtension(selectedTableB));
+		tableQuery.add(selectedKeyB);
+		tableQuery.add(selectedJoin);
 
-        return tableQuery;
-    }
+		return tableQuery;
+	}
 
-    /**
-     * Get selectedTableA.
-     *
-     * @return selectedTableA
-     */
-    public String getSelectedTableA() {
-        return selectedTableA;
-    }
+	/**
+	 * Get selectedTableA.
+	 *
+	 * @return selectedTableA
+	 */
+	public String getSelectedTableA() {
+		return selectedTableA;
+	}
 
-    /**
-     * Set selectedTableA.
-     *
-     * @param selectedTableA selectedTableA
-     */
-    public void setSelectedTableA(String selectedTableA) {
-        this.selectedTableA = selectedTableA;
-    }
+	/**
+	 * Set selectedTableA.
+	 *
+	 * @param selectedTableA selectedTableA
+	 */
+	public void setSelectedTableA(String selectedTableA) {
+		this.selectedTableA = selectedTableA;
+	}
 
-    /**
-     * Get selectedTableB.
-     *
-     * @return selectedTableB
-     */
-    public String getSelectedTableB() {
-        return selectedTableB;
-    }
+	/**
+	 * Get selectedTableB.
+	 *
+	 * @return selectedTableB
+	 */
+	public String getSelectedTableB() {
+		return selectedTableB;
+	}
 
-    /**
-     * Set selectedTableB.
-     *
-     * @param selectedTableB selectedTableB
-     */
-    public void setSelectedTableB(String selectedTableB) {
-        this.selectedTableB = selectedTableB;
-    }
+	/**
+	 * Set selectedTableB.
+	 *
+	 * @param selectedTableB selectedTableB
+	 */
+	public void setSelectedTableB(String selectedTableB) {
+		this.selectedTableB = selectedTableB;
+	}
 
-    /**
-     * Get selectedJoin.
-     *
-     * @return selectedJoin
-     */
-    public String getSelectedJoin() {
-        return selectedJoin;
-    }
+	/**
+	 * Get selectedJoin.
+	 *
+	 * @return selectedJoin
+	 */
+	public String getSelectedJoin() {
+		return selectedJoin;
+	}
 
-    /**
-     * Set selectedJoin.
-     *
-     * @param selectedJoin selectedJoin
-     */
-    public void setSelectedJoin(String selectedJoin) {
-        this.selectedJoin = selectedJoin;
-    }
+	/**
+	 * Set selectedJoin.
+	 *
+	 * @param selectedJoin selectedJoin
+	 */
+	public void setSelectedJoin(String selectedJoin) {
+		this.selectedJoin = selectedJoin;
+	}
 
-    /**
-     * Get selectedKeyA.
-     *
-     * @return selectedKeyA
-     */
-    public String getSelectedKeyA() {
-        return selectedKeyA;
-    }
+	/**
+	 * Get selectedKeyA.
+	 *
+	 * @return selectedKeyA
+	 */
+	public String getSelectedKeyA() {
+		return selectedKeyA;
+	}
 
-    /**
-     * Set selectedKeyA.
-     *
-     * @param selectedKeyA selectedKeyA
-     */
-    public void setSelectedKeyA(String selectedKeyA) {
-        this.selectedKeyA = selectedKeyA;
-    }
+	/**
+	 * Set selectedKeyA.
+	 *
+	 * @param selectedKeyA selectedKeyA
+	 */
+	public void setSelectedKeyA(String selectedKeyA) {
+		this.selectedKeyA = selectedKeyA;
+	}
 
-    /**
-     * Get selectedKeyB.
-     *
-     * @return selectedKeyB
-     */
-    public String getSelectedKeyB() {
-        return selectedKeyB;
-    }
+	/**
+	 * Get selectedKeyB.
+	 *
+	 * @return selectedKeyB
+	 */
+	public String getSelectedKeyB() {
+		return selectedKeyB;
+	}
 
-    /**
-     * Set selectedKeyB.
-     *
-     * @param selectedKeyB selectedKeyB
-     */
-    public void setSelectedKeyB(String selectedKeyB) {
-        this.selectedKeyB = selectedKeyB;
-    }
+	/**
+	 * Set selectedKeyB.
+	 *
+	 * @param selectedKeyB selectedKeyB
+	 */
+	public void setSelectedKeyB(String selectedKeyB) {
+		this.selectedKeyB = selectedKeyB;
+	}
 
-    /**
-     * Get tableAKeys.
-     *
-     * @return tableAKeys
-     */
-    public List<String> getTableAKeys() {
-        return tableAKeys;
-    }
+	/**
+	 * Get tableAKeys.
+	 *
+	 * @return tableAKeys
+	 */
+	public List<String> getTableAKeys() {
+		return tableAKeys;
+	}
 
-    /**
-     * Set tableAKeys.
-     *
-     * @param tableAKeys tableAKeys
-     */
-    public void setTableAKeys(List<String> tableAKeys) {
-        this.tableAKeys = tableAKeys;
-    }
+	/**
+	 * Set tableAKeys.
+	 *
+	 * @param tableAKeys tableAKeys
+	 */
+	public void setTableAKeys(List<String> tableAKeys) {
+		this.tableAKeys = tableAKeys;
+	}
 
-    /**
-     * Get tableBKeys.
-     *
-     * @return tableBKeys
-     */
-    public List<String> getTableBKeys() {
-        return tableBKeys;
-    }
+	/**
+	 * Get tableBKeys.
+	 *
+	 * @return tableBKeys
+	 */
+	public List<String> getTableBKeys() {
+		return tableBKeys;
+	}
 
-    /**
-     * Set tableBKeys.
-     *
-     * @param tableBKeys tableBKeys
-     */
-    public void setTableBKeys(List<String> tableBKeys) {
-        this.tableBKeys = tableBKeys;
-    }
+	/**
+	 * Set tableBKeys.
+	 *
+	 * @param tableBKeys tableBKeys
+	 */
+	public void setTableBKeys(List<String> tableBKeys) {
+		this.tableBKeys = tableBKeys;
+	}
 
 }
